c4aeac81d6ecf54658f877a6a16ee995ceef1c9f
Richa Sharma <onlinericha19@gmail.com>
2019-08-11 19:32:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import SeasonDisplay from'./SeasonDisplay';
+import Loader from './Loader';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+class App extends React.Component {
+
+state = {lat: null, errorMessage: ''};
+
+componentDidMount () {
+
+  window.navigator.geolocation.getCurrentPosition(
+    position => this.setState({ lat: position.coords.latitude }),
+    err => this.setState({errorMessage: err.message})
   );
+
 }
+
+renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div> Error: {this.state.errorMessage}</div>
+   }
+
+   if (!this.state.errorMessage && this.state.lat){
+      return <SeasonDisplay lat = {this.state.lat}/>
+    }
+   return <Loader message = "Please accept Location request" />;
+
+
+}
+
+
+
+  render() {
+
+    return (
+      <div className = "border red">
+      {this.renderContent()}
+      </div>
+
+    )
+
+
+  }
+}
+
+
 
 export default App;
---
+++ src/Loader.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Loader = (props) => {
+return (
+	<div class="ui active dimmer">
+    	<div class="ui text loader">{props.message}</div>
+  	</div>
+  );
+};
+
+Loader.defaultProps = {
+	message: "Loading.."
+}
+
+export default Loader;
